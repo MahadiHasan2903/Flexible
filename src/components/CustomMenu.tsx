e5826a7fcb70057e46bca45ca12493073388afc3
MD. Mahadi Hasan <12mahadihasan@gmail.com>
2023-10-22 11:10:14 -0700

    updated

---
+++ src/components/CustomMenu.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomMenu = () => {
+  return <div>CustomMenu</div>;
+};
+
+export default CustomMenu;
---
+++ src/components/RelatedProjects.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RelatedProjects = () => {
+  return <div>RelatedProjects</div>;
+};
+
+export default RelatedProjects;
